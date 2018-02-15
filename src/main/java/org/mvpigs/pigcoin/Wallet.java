5afd799707b66039f6fbf31c86e59374f0576549
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-02-15 17:16:42 +0100

    Casos test mostrar2wallets pasado

--- src/main/java/org/mvpigs/pigcoin/Wallet.java
+++ src/main/java/org/mvpigs/pigcoin/Wallet.java
@@ -5,6 +5,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class Wallet {
+
     private PublicKey address;
     private PrivateKey sKey;
     private double total_input;
@@ -49,5 +50,27 @@ public class Wallet {
 
     public void setsKey(PrivateKey sKey) {
         this.sKey = sKey;
+    }
+
+    public double getTotal_input(){
+        return this.total_input;
+    }
+
+    public void setTotal_input(double total_input) {
+        this.total_input = total_input;
+    }
+    public double getTotal_output(){
+        return this.total_output;
+    }
+    public void setTotal_output(double total_output) {
+        this.total_output = total_output;
+    }
+
+
+    public double getBalance() {
+        return this.balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 }
--- src/test/java/org/mvpigs/testAppJuanmi.java
+++ src/test/java/org/mvpigs/testAppJuanmi.java
@@ -12,8 +12,20 @@ import java.security.KeyPair;
 
 public class testAppJuanmi {
 
+    private Wallet wallet_1;
+    private Wallet wallet_2;
+
+    @Before
+    public void testCreacionWallets(){
+        wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+        wallet_2 = new Wallet();
+        wallet_2.generateKeyPair();
+    }
+
+
     @Test
-    public void crearWallet1(){
+    public void testCrearWallet1(){
         Wallet wallet_1 = new Wallet();
         KeyPair pair = GenSig.generateKeyPair();
         wallet_1.setSK(pair.getPrivate());
@@ -22,11 +34,29 @@ public class testAppJuanmi {
     }
 
     @Test
-    public void crearWallet2(){
+    public void testCrearWallet2(){
         Wallet wallet_2 = new Wallet();
         wallet_2.generateKeyPair();
 
     }
+
+    @Test
+    public void visualizarWallets(){
+
+        //Wallet 1
+        assertEquals(wallet_1.toString(), "\nWallet = "+ wallet_1.getAddress().hashCode()+
+                "\nTotal input = "+wallet_1.getTotal_input()+
+                "\nTotal output = "+wallet_1.getTotal_output()+
+                "\nBalance = "+wallet_1.getBalance()+"\n");
+
+        //Wallet 2
+        assertEquals(wallet_2.toString(), "\nWallet = "+ wallet_2.getAddress().hashCode()+
+                "\nTotal input = "+wallet_2.getTotal_input()+
+                "\nTotal output = "+wallet_2.getTotal_output()+
+                "\nBalance = "+wallet_2.getBalance()+"\n");
+    }
+
+
 
 
 }
